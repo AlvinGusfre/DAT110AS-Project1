e751172f1731c7a2105733634c931f4c3cbd0d32
Alvin <570958@stud.hvl.no>
2020-01-30 11:32:56 +0100

    Heihå

--- dat110-project1/src/no/hvl/dat110/messaging/Connection.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Connection.java
@@ -31,7 +31,13 @@ public class Connection {
 	}
 
 	public void send(Message message) {
-
+		
+		try {
+			outStream.write(message.encapsulate());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		// TODO
 		// encapsulate the data contained in the message and write to the output stream
 		// Hint: use the encapsulate method on the message
@@ -44,6 +50,7 @@ public class Connection {
 		Message message;
 		byte[] recvbuf;
 
+		
 		// TODO
 		// read a segment (128 bytes) from the input stream and decapsulate into message
 		// Hint: create a new Message object and use the decapsulate method
--- dat110-project1/src/no/hvl/dat110/messaging/Message.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -9,7 +9,11 @@ public class Message {
 	private byte[] payload;
 
 	public Message(byte[] payload) {
-		this.payload = payload; // TODO: check for length within boundary
+		if (payload.length <= 127) {
+			this.payload = payload; // TODO: check for length within boundary
+		} else {
+			return;
+		}
 	}
 
 	public Message() {
@@ -17,31 +21,40 @@ public class Message {
 	}
 
 	public byte[] getData() {
-		return this.payload; 
+		return this.payload;
 	}
 
 	public byte[] encapsulate() {
-		
-		byte[] encoded = null;
-		
+
+		byte[] encoded = new byte[128];
+
+		encoded[0] = (byte) payload.length;
+
+		for (int i = 1; i < payload.length - 1; i++) 
+		{
+			encoded[i] = payload[i - 1];
+		}
+
 		// TODO
 		// encapulate/encode the payload of this message in the
 		// encoded byte array according to message format
-		
-		if (true)
-		   throw new UnsupportedOperationException(TODO.method());
 
 		return encoded;
-		
+
 	}
 
 	public void decapsulate(byte[] received) {
-
+		
+		for (int i = 1; i < (int)received[0]; i++)
+		{
+			payload[i-1] = received[i];
+		}
+		
+		
 		// TODO
-		// decapsulate the data contained in the received byte array and store it 
+		// decapsulate the data contained in the received byte array and store it
 		// in the payload of this message
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+
 	}
 }
